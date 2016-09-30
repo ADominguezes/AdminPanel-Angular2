775921fe1d429a2a627be95905937084e4ae0a96
Adominguezes <alberto@adominguez.es>
2016-09-30 19:21:13 +0200

    estilos y configuración Sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -6,9 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+  /**
+   * get the user information of a JSON 
+   */
+  userInformation = [
+    {
+      name: "Jhon",
+      lastName: "Doe",
+      imgBackground: "background-1-200x100.jpg",
+      imgUser: "1.jpg"
+    }
+  ]
+  /**
+   * save the user information in the variables
+   */
+  name: string;
+  lastName: string;
+  imgBackground: string;
+  urlImgUser: string;
+  styleBackground: string;
+  hiddeInfoUser: boolean = true;
 
-  constructor() { }
-
+  constructor() {
+    /**
+     * We prepare the information for the front end
+     */
+    this.name = this.userInformation[0].name;
+    this.lastName = this.userInformation[0].lastName;
+    this.imgBackground = this.userInformation[0].imgBackground;
+    this.styleBackground = "url(assets/users/" + this.imgBackground + ") center no-repeat";
+    this.urlImgUser = "assets/users/" + this.userInformation[0].imgUser;
+  }
+  /**
+   * show user info
+   */
+  showInfo() {
+    this.hiddeInfoUser = !this.hiddeInfoUser
+  }
   ngOnInit() {
   }
 
